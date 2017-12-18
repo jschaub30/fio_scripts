cee53ba9b76fcc11d63a8f0c65a42f782dda0ee1
Jeremy Schaub <jschaub@statestreet.com>
2017-12-18 20:18:08 +0000

    Added Ubuntu repos

--- install.sh
+++ install.sh
@@ -15,10 +15,11 @@ if [ $OS == "CENTOS" ]; then
   sudo yum install R
 fi
 if [ $OS == "UBUNTU" ]; then
-  sudo apt-get install epel-release -y
+  echo "deb https://cran.revolutionanalytics.com/bin/linux/ubuntu xenial/" | sudo tee /etc/apt/sources.list.d/r.list
+  sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E084DAB9
   sudo apt-get update
   sudo apt-get install fio dstat sysstat htop -y
-  sudo apt-get install r-base
+  sudo apt-get install r-base -y
 fi
 
 echo ==== Installing R packages ====
