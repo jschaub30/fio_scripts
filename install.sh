20816279839d4ae5160e410b378116b0bc741e3a
Jeremy Schaub <jschaub@statestreet.com>
2017-12-20 13:07:26 -0600

    Added flag

--- install.sh
+++ install.sh
@@ -11,14 +11,14 @@ which apt-get
 echo ==== Installing linux packages ====
 if [ $OS == "CENTOS" ]; then
   sudo yum install epel-release -y
-  sudo yum update
+  sudo yum update -y
   sudo yum install fio dstat sysstat htop -y
   sudo yum install R -y
 fi
 if [ $OS == "UBUNTU" ]; then
   echo "deb https://cran.revolutionanalytics.com/bin/linux/ubuntu xenial/" | sudo tee /etc/apt/sources.list.d/r.list
   sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E084DAB9
-  sudo apt-get update
+  sudo apt-get update -y
   sudo apt-get install fio dstat sysstat htop -y
   sudo apt-get install r-base -y
 fi
